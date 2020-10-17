f9b1c731d6b28a1166a9e6898df646bfeb564123
erwan-png <erwan.v@free.fr>
2020-10-17 17:10:28 +0200

    get handled

--- src/http/server/WebServer.java
+++ src/http/server/WebServer.java
@@ -3,10 +3,17 @@
 package http.server;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Example program from Chapter 1 Programming Spiders, Bots and Aggregators in
@@ -19,6 +26,10 @@ import java.net.Socket;
  * @version 1.0
  */
 public class WebServer {
+
+  protected void lireEnTete() {
+      
+  }
 
   /**
    * WebServer constructor.
@@ -52,18 +63,68 @@ public class WebServer {
         // blank line signals the end of the client HTTP
         // headers.
         String str = ".";
+        String requestLine = "";
+        Map<String,String> headerFields = new HashMap<>();
+        String body = "";
+
         while (str != null && !str.equals("")) {
           str = in.readLine();
-          if (str.contains("GET")) {
-            out.println("Nous recevons une requête GET");
+          if(requestLine.equals(""))
+            requestLine = str;
+          else {
+            int indexFin = str.indexOf(":");
+            if(indexFin != -1)
+              headerFields.put(str.substring(0, indexFin).trim(), str.substring(indexFin+1).trim());
+          }
+        }
+
+        if(headerFields.containsKey("Content-Length")) {
+          int nbrCharac = Integer.parseInt(headerFields.get("Content-Length"));
+          int count = 0;
+          int read;
+          while( (read = in.read()) != -1 && count < nbrCharac) {
+            body += (char) read;
+          }
+        }
+
+        //System.out.println("Ligne d'en-tête : " + requestLine);
+
+        /*System.out.println();
+        System.out.println("En-têtes : \n" + headerFields);
+
+        System.out.println();
+        System.out.println("Fin de l'en-tête");
+        System.out.println();
+
+        System.out.println("Corps : " + body);*/
+
+        if(requestLine.contains("GET")) {
+          //System.out.println("On a un GET");
+          String[] params = requestLine.split(" ");
+          //System.out.println("Param : " + Arrays.toString(params));
+          int indexStart = params[1].indexOf("/");
+          String path = params[1].substring(indexStart+1).trim();
+          if (!path.isEmpty()) {
+            File file = new File(path);
+            if(!file.exists())
+              out.write("HTTP ERROR 404 : The requested file was not found on the server\n");
+            else {
+              FileReader fr = new FileReader(file);
+              BufferedReader bfr = new BufferedReader(fr);
+              String line;
+              while ((line = bfr.readLine()) != null) {
+                  out.write(line);
+              }
+              bfr.close();
+            }
           }
         }
 
         // Send the response
         // Send the headers
-        out.println("HTTP/1.0 200 OK");
-        out.println("Content-Type: text/html");
-        out.println("Server: Bot");
+        //out.println("HTTP/1.0 200 OK");
+        //out.println("Content-Type: text/html");
+        //out.println("Server: Bot");
         // this blank line signals the end of the headers
         out.println("");
         // Send the HTML page
